9596f535868647feb8d02a7eb00a071b12d98d8f
Danila Kuriabov <dkuriabov@mail.ru>
2022-11-07 10:49:05 +0300

    Cache implemented

--- src/main/java/LRUCacheImpl.kt
+++ src/main/java/LRUCacheImpl.kt
@@ -1,17 +1,78 @@
-class LRUCacheImpl<KeyType, ValueType>(capacity: Int): LRUCache<KeyType, ValueType> {
+import java.security.Key
+
+class LRUCacheImpl<KeyType, ValueType>(private val capacity: Int): LRUCache<KeyType, ValueType> {
+    private val values: HashMap<KeyType, Node<KeyType, ValueType>> = hashMapOf()
+    private var head: Node<KeyType, ValueType>? = null
+    private var tail: Node<KeyType, ValueType>? = null
+
     init {
-        assert(capacity > 0) {
-            println(" Capacity must be positive ")
-        }
+        assert(capacity > 0) { "Capacity must be positive" }
     }
 
     override fun cache(key: KeyType, value: ValueType) {
-        TODO("Not yet implemented")
+        removeNode(key)
+        addNewNode(Node(key, value))
+        assert(head?.value == value) { "Wrong value priority" }
+        assert(values.size <= capacity) { "Cache overflow" }
     }
 
     override fun get(key: KeyType): ValueType? {
-        TODO("Not yet implemented")
+        val size = values.size
+
+        val value = values[key]
+
+        return if (value != null) {
+            cache(key, value.value)
+            assert(size == values.size) { "Values count should not change" }
+            value.value
+        } else {
+            null
+        }
+
     }
 
-    private class Node()
+    private fun addNewNode(node: Node<KeyType, ValueType>) {
+        assert(!values.containsKey(node.key)) { " Can't add new node if it exists " }
+
+        if (values.size == capacity) {
+            values.remove(tail?.key)
+            tail = tail?.previous
+        }
+
+        node.next = head
+        node.previous = null
+        head?.previous = node
+        head = node
+
+        if (values.size == 0) {
+            tail = node
+        }
+
+        values[node.key] = node
+    }
+
+    private fun removeNode(key: KeyType) {
+        val removedNode = values.remove(key)
+
+//      previous  <->  node  <->  next
+//                <------------>
+
+        if (removedNode == tail) {
+            tail = removedNode?.previous
+        }
+
+        val previous = removedNode?.previous
+        val next = removedNode?.next
+
+        previous?.next = next
+        next?.previous = previous
+
+    }
+
+    data class Node<KeyType, ValueType>(
+        val key: KeyType,
+        var value: ValueType,
+        var previous: Node<KeyType, ValueType>? = null,
+        var next: Node<KeyType, ValueType>? = null,
+    )
 }
--- src/test/java/LRUCacheImplTest.kt
+++ src/test/java/LRUCacheImplTest.kt
@@ -5,7 +5,47 @@ internal class LRUCacheImplTest {
 
     @Test
     fun `added value is saved`() {
-        val cache = LRUCacheImpl<Int, String>(1)
-
+        val cache: LRUCache<Int, String> = LRUCacheImpl(1)
+        cache.cache(0, "0")
+        assertEquals("0", cache.get(0))
     }
+
+    @Test
+    fun `create cache with negative capacity`() {
+        assertThrows(AssertionError::class.java) { LRUCacheImpl<Int, String>(-1) }
+    }
+
+    @Test
+    fun `get not existing value`() {
+        val cache = LRUCacheImpl<Int, String>(4)
+        assertEquals(null, cache.get(0))
+    }
+
+    @Test
+    fun `clears overhead correctly`() {
+        val cache = LRUCacheImpl<Int, String>(2)
+        cache.cache(0, "0")
+        cache.cache(1, "1")
+        cache.cache(2, "2")
+        assertEquals(null, cache.get(0))
+        assertEquals("1", cache.get(1))
+        assertEquals("2", cache.get(2))
+    }
+
+
+    @Test
+    fun `update frequently used keys priority`() {
+        val cache = LRUCacheImpl<Int, String>(3)
+        cache.cache(0, "0")
+        cache.cache(1, "1")
+        cache.cache(0, "0-new")
+        cache.cache(2, "2")
+        cache.cache(3, "3")
+
+        assertEquals("0-new", cache.get(0))
+        assertEquals(null, cache.get(1))
+        assertEquals("2", cache.get(2))
+        assertEquals("3", cache.get(3))
+    }
+
 }
